f1a28ee984c8f6482f0ba33b469b390770eb331f
Aman Mishra <12defence2005@gmail.com>
2024-06-11 20:14:02 +0530

    updatemm

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './company/Home';
 import Login from './company/Login';
 import OtpVerify from './company/Otpverify';
-<<<<<<< HEAD
-import Usermanage from './company/Usermanage';
-=======
 import Navbar from './company/Navbar';
->>>>>>> 48ab1cb36dadcb368849a7e0b1daab88979203f9
+import TEAM from './company/Team';
 const App = () => {
   return (
     <div>
@@ -17,11 +14,8 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="Login" element={<Login />} />
           <Route path="otpverify" element={<OtpVerify />} />
-<<<<<<< HEAD
-          <Route path="usermanage" element={<Usermanage/>} />
-=======
           <Route path="navbar" element={<Navbar/>} />
->>>>>>> 48ab1cb36dadcb368849a7e0b1daab88979203f9
+          <Route path="team" element={<TEAM/>} />
       </Routes>
     </BrowserRouter>
     </div>
