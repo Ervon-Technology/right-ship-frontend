976ca3545622c36801b27b74605fe5058b7bcea7
Sambit  Pattanaik <pattanaiksambit80@gmail.com>
2024-06-12 20:04:50 +0530

    sidebar

--- src/company/Sidebar.js
+++ src/company/Sidebar.js
@@ -8,14 +8,14 @@ export const Sidebar = ({ children }) => {
   return (
     <div id="sidebar">
       <div className='h-screen'>
-        <nav className="h-full flex flex-col bg-customBlue border-r shadow-sm">
-          <div className="p-5 pb-2 flex justify-between items-center">
+        <nav className="h-full flex flex-col bg-customBlue border-r shadow-sm ">
+          <div className="mt-1 p-5 ml-0.5 flex justify-between items-center ">
             <button onClick={() => setExpanded((current) => !current)} className="text-black bg-none p-1 rounded flex">
-              {expanded ? <X color='white' size={20} /> : <Menu color='white' size-20/>}<span className={`overflow-hidden text-sm text-white transition-all ${expanded ? " ml-3 " : " hidden "}`}>Collapse</span>
+              {expanded ? <X color='white' className='mt-1' size={20}/> : <Menu color='white' className='' size-20/>}<span className={`overflow-hidden text-sm text-white transition-all ${expanded ? " ml-3 mt-1 " : " hidden "}`}>Collapse</span>
             </button>
           </div>
           <SidebarContext.Provider value={expanded}>
-            <ul className='flex-1 px-3'>{children}</ul>
+            <ul className='flex-1 px-3 '>{children}</ul>
           </SidebarContext.Provider>
         </nav>
       </div>
@@ -26,8 +26,8 @@ export const Sidebar = ({ children }) => {
 export const SidebarItem = ({ icon, text }) => {
   const expanded = useContext(SidebarContext);
   return (
-    <li className='relative flex items-center p-3 my-1 font-medium rounded-md cursor-pointer hover:bg-slate-500'>
-      <span className='text-white my-1.5'>{icon}</span>
+    <li className='relative flex items-center p-3 my-1 font-medium rounded-md cursor-pointer hover:bg-slate-500 transition duration-300 ease-in-out'>
+      <span className='text-white my-1.5 ml-0.5'>{icon}</span>
       <span className={`overflow-hidden text-sm text-white transition-all ${expanded ? "w-52 ml-3 text-nowrap py-1" : " w-0 text-nowrap"}`}>{text}</span>
     </li>
   );
