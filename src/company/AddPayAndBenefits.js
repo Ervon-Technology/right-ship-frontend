386de2a1ea055068b67546d8602baaf45007fe49
Sambit  Pattanaik <pattanaiksambit80@gmail.com>
2024-06-14 20:53:03 +0530

    changes

--- src/company/AddPayAndBenefits.js
+++ src/company/AddPayAndBenefits.js
@@ -19,15 +19,15 @@ const AddPayAndBenefits = () => {
     setJobDescription(event.target.value);
   };
   return (
-    <div className="max-w-3xl mx-auto mt-10">
+    <div className="max-w-3xl mx-auto my-6">
         <div className="py-10 bg-customSky1 flex justify-around items-center w-full max-w-4xl px-6">
             <h1 className="text-3xl font-bold">Add Pay and Benefits</h1>
             <img src={Ship} alt="ship" height={120} width={120} />
         </div>
         <div className="bg-customSky2 shadow-md rounded-lg p-14 ">
             <div className="mb-10 bg-white">
-                <h5 className="text-lg font-semibold mb-4">Benefits</h5>
-                <div className="grid grid-cols-3 gap-2 border-solid border-2 border-sky-200 rounded-lg p-4">
+                <h5 className="text-lg font-bold mb-4">Benefits</h5>
+                <div className="grid grid-cols-3 gap-2 border-solid border-2 border-sky-200 rounded-lg p-6 px-8">
                     {benefitsList.map((benefit, index) => (
                     <div key={index} className="flex items-center">
                         <input type="checkbox" id={`benefit-${index}`} name="benefits" value={benefit} className="mr-1" />
@@ -38,12 +38,12 @@ const AddPayAndBenefits = () => {
             </div>
 
             <div className="mb-6">
-              <h5 className="text-lg font-semibold mb-4">Add Job Description</h5>
+              <h5 className="text-lg font-bold mb-4">Add Job Description</h5>
                 <textarea
                   value={jobDescription}
                   onChange={handleDescriptionChange}
                   placeholder="Enter job description here"
-                  className="w-full p-2 border border-gray-300 rounded-md h-40 mb-4"
+                  className="w-full p-2 border border-sky-200 rounded-md h-40 mb-4"
                 ></textarea>
                 <div className="flex justify-between mt-">
                     <Link to="/add-job-basics" className="bg-white hover:bg-gray-100 text-gray-700 font-bold py-2 px-4 rounded-md shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">&larr; Back</Link>
