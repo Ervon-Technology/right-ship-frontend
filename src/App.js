6a6931c32ec633687b7875b7017c223f91ad7ccd
Sambit  Pattanaik <pattanaiksambit80@gmail.com>
2024-08-23 01:45:38 +0530

    Structure change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import CompanyLayout from '../src/component/layout/companyLayout';
 import HomePage from './job_seeker/landingpage/Home';
 import NotFoundPage from './pageNotFound';
 import CompanyOtpAuth from './company/login/login';
-import RegistrationForm from './company/RegistrationForm';
+import RegistrationForm from './company/registrationform';
 import EmployeeOtpAuth from './job_seeker/login/employeelogin';
 import EmployeeSignup from './job_seeker/signup/Signup';
 import VerifySignupOtp from './job_seeker/signup/VerifySignupOtp';
@@ -29,7 +29,7 @@ import JobPostList from './company/job/jobPostList';
 import CandidatesTable from './company/candidates/candidateList';
 import CandidateDetail from './company/candidates/candidateDetail';
 import JobDetail from './company/job/jobdetail';
-import CompanyManageUsers from './company/ManageUsers';
+import CompanyManageUsers from './company/manageusers';
 import CompanyProfile from './company/setting';
 import Setting from './job_seeker/setting/Setting'
 import JobBoard from './job_seeker/jobs/MyJobs';
