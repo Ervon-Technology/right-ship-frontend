425fd3f75e63ad38e36e82a1164d85987121e951
rishsingh7 <rishabsiingh07@gmail.com>
2024-10-08 12:24:26 +0530

    Revert "Copy url button added"

--- src/company/job/jobdetail.js
+++ src/company/job/jobdetail.js
@@ -3,7 +3,6 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 import { useSelector, useDispatch } from 'react-redux';
-import CopyUrlButton from '../../component/copyButton';
 
 const JobDetail = () => {
 
@@ -12,7 +11,7 @@ const JobDetail = () => {
     const [company, setCompany] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
-
+    
     const user = useSelector(state => state.auth.user);
 
     useEffect(() => {
@@ -55,13 +54,13 @@ const JobDetail = () => {
                 const response = await axios.post(`${process.env.REACT_APP_API_URL}/company/get`, requestData);
 
                 if (response.data.code === 200) {
-                    setCompany(response.data.data);
+                    setCompany(response.data.data); 
                     setLoading(false);
                 } else {
                     setError('Failed to fetch job details.');
                     setLoading(false);
                 }
-
+               
                 return true;
             } catch (error) {
                 setError('An error occurred while fetching job details.', error);
@@ -83,22 +82,19 @@ const JobDetail = () => {
     }
     return (
         <div className='p-6'>
-            <div className="bg-white shadow-md rounded-lg p-6 mb-6">
-                <div className='flex flex-row justify-between align-center'>
-                    <h1 className="text-3xl font-semibold text-gray-800 mb-4">{job.hiring_for.join(', ')}</h1>
-                    <CopyUrlButton />
-                </div>
-                <div className="text-gray-700">
-                    <p className="text-lg font-medium">Company Name: <span className="font-normal">{job.company_name}</span></p>
-                    <p className="text-lg font-medium">Mobile No: <span className="font-normal">{job.mobile_no}</span></p>
-                    <p className="text-lg font-medium">Open Positions: <span className="font-normal">{job.open_positions.join(', ')}</span></p>
-                    <p className="text-lg font-medium">Status: <span className="font-normal">{job.status}</span></p>
-                    <p className="text-lg font-medium">Posted On: <span className="font-normal">{new Date(job.created_date).toLocaleDateString('en-IN', { day: '2-digit', month: 'short', year: 'numeric' })}</span></p>
-                    {job.description && (
-                        <p className="mt-6 text-base text-gray-600">{job.description}</p>
-                    )}
-                </div>
+        <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+            <h1 className="text-3xl font-semibold text-gray-800 mb-4">{job.hiring_for.join(', ')}</h1>
+            <div className="text-gray-700">
+                <p className="text-lg font-medium">Company Name: <span className="font-normal">{job.company_name}</span></p>
+                <p className="text-lg font-medium">Mobile No: <span className="font-normal">{job.mobile_no}</span></p>
+                <p className="text-lg font-medium">Open Positions: <span className="font-normal">{job.open_positions.join(', ')}</span></p>
+                <p className="text-lg font-medium">Status: <span className="font-normal">{job.status}</span></p>
+                <p className="text-lg font-medium">Posted On: <span className="font-normal">{new Date(job.created_date).toLocaleDateString('en-IN', { day: '2-digit', month: 'short', year: 'numeric' })}</span></p>
+                {job.description && (
+                    <p className="mt-6 text-base text-gray-600">{job.description}</p>
+                )}
             </div>
+        </div>
 
         </div>
     );
