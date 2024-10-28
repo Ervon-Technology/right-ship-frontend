bc55ed405ebaa9073425322ee490563da98788d0
rishsingh7 <rishabsiingh07@gmail.com>
2024-10-28 22:59:26 +0530

    Email issue fixed

--- src/job_seeker/login/employeeloginform.jsx
+++ src/job_seeker/login/employeeloginform.jsx
@@ -14,20 +14,8 @@ const MobileNumberForm = ({ onOtpRequested }) => {
     const verifyUserExists = async () => {
         try {
             const isEmail = contactInfo.includes('@');
-            let response
-            if(isEmail){
-                response = await axios.post(`${process.env.REACT_APP_API_URL}/user/details`, {
-                    email: contactInfo,
-                    user_type: 'employee' // Assuming 'employee' is the user type; modify as needed
-                });
-            }
-            else{
-                response = await axios.post(`${process.env.REACT_APP_API_URL}/user/details`, {
-                    mobile_no: contactInfo,
-                    user_type: 'employee' // Assuming 'employee' is the user type; modify as needed
-                });
-            }
-
+                const payload = isEmail ? { email: contactInfo, user_type: 'employee' } : { mobile_no: contactInfo, user_type: 'employee' };
+                const response = await axios.post(`${process.env.REACT_APP_API_URL}/user/details`, payload);
             return response.data; // Assuming 'exists' is a field that returns true if the user exists
         } catch (error) {
             console.error('Error verifying user existence:', error);
