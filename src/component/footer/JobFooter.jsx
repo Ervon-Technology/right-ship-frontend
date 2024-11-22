660454cf163faba6333112bb875ebaf8d0bc5e4e
rishsingh7 <rishabsiingh07@gmail.com>
2024-11-22 15:04:06 +0530

    Fixed footer routes

--- src/component/footer/JobFooter.jsx
+++ src/component/footer/JobFooter.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { FaFacebook, FaLinkedin, FaInstagram, FaYoutube } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const JobFooter = () => {
   return (
     <footer className="bg-gray-100 text-black border border-t-2 py-3 px-12 w-full">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="flex space-x-4 text-sm mb-4 md:mb-0">
-          <a href='/' className="text-sm mb-4 md:mb-0">© 2024 Rightships</a>
-          <a href="/privacy-policy" target="_blank" rel="noopener noreferrer" className="hover:underline">Privacy Policy</a>
-          <a href="/terms" target="_blank" rel="noopener noreferrer" className="hover:underline">Terms</a>
-          <a href="/contact-us" target="_blank" rel="noopener noreferrer" className="hover:underline">Contact US</a>
-          <a href="https://www.ervon.tech/" target="_blank" rel="noopener noreferrer" className="hover:underline">Power by Ervon Technology PVT LTD</a>
+          <Link to='/' className="text-sm mb-4 md:mb-0">© 2024 Rightships</Link>
+          <Link to="/privacy-policy" target="_blank" rel="noopener noreferrer" className="hover:underline">Privacy Policy</Link>
+          <Link to="/terms" target="_blank" rel="noopener noreferrer" className="hover:underline">Terms</Link>
+          <Link to="/contact-us" target="_blank" rel="noopener noreferrer" className="hover:underline">Contact US</Link>
+          <Link to="https://www.ervon.tech/" target="_blank" rel="noopener noreferrer" className="hover:underline">Power by Ervon Technology PVT LTD</Link>
         </div>
         <div className="flex space-x-4">
           <a href="https://www.facebook.com/profile.php?id=61565469260372" target="_blank" rel="noopener noreferrer">
